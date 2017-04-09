b6c713f5f2ebc0ca3886a62affabbc0a850f0809
felwe <igsongfelwe@gmai.com>
2017-04-09 20:35:19 +0100

    bonsoir

---
+++ src/applicationtp/InterfaceDessin.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package applicationtp;
+
+/**
+ *
+ * @author ko
+ */
+public interface InterfaceDessin {
+    public void dessin(FigureGeometrique d);
+    
+}
